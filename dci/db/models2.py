d1c3cd8c81258ade24a9e805aafd9e562e38c8ef
ylamgarchal <yassine.lamgarchal@redhat.com>
2021-11-28 22:50:06 +0100

    jobs: move to orm
    Change-Id: I86021493ae3e326b1a30aa7fffa09a364d70d541

--- dci/db/models2.py
+++ dci/db/models2.py
@@ -414,6 +414,11 @@ class Job(dci_declarative.Mixin, Base):
     tags = sa.Column(pg.ARRAY(sa.Text), default=[])
     data = sa.Column(sa_utils.JSONType, default={})
     components = sa_orm.relationship('Component', secondary=JOIN_JOBS_COMPONENTS, back_populates='jobs')
+    results = sa_orm.relationship('TestsResult')
+    remoteci = sa_orm.relationship('Remoteci')
+    topic = sa_orm.relationship('Topic')
+    team = sa_orm.relationship('Team')
+    jobstates = sa_orm.relationship('Jobstate')
 
 
 class Jobstate(dci_declarative.Mixin, Base):
@@ -452,7 +457,7 @@ class Tests(dci_declarative.Mixin, Base):
     state = sa.Column(STATES, default='active')
 
 
-class TestsResults(dci_declarative.Mixin, Base):
+class TestsResult(dci_declarative.Mixin, Base):
     __tablename__ = 'tests_results'
     __table_args = (sa.Index('tests_results_job_id_idx', 'job_id'),
                     sa.Index('tests_results_file_id_idx', 'file_id'))
