9e9ec02b14c7213c8b6ddb74b8b642742296ae84
ylamgarchal <yassine.lamgarchal@redhat.com>
2021-12-16 02:32:54 +0100

    add ability to get resources from a given timestamp
      GET /resources?created_after=<date-iso-format>
      GET /resources?updated_after=<date-iso-format>
    
    Change-Id: I264bfa8c4d8f6f0944f68d750c85a94f69b5a109

--- dci/common/args.py
+++ dci/common/args.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 
 def _get_csv(element, dict):
@@ -15,6 +16,17 @@ def _get_int(element, dict):
     return e
 
 
+def _get_date(element, dict):
+    e = dict.get(element, None)
+    if e:
+        try:
+            return datetime.strptime(e, "%Y-%m-%dT%H:%M:%S.%f")
+        except ValueError:
+            pass
+
+        return datetime.strptime(e, "%Y-%m-%d")
+
+
 def parse_args(args):
     return {
         "limit": _get_int("limit", args),
@@ -22,4 +34,6 @@ def parse_args(args):
         "sort": _get_csv("sort", args),
         "where": _get_csv("where", args),
         "embed": _get_csv("embed", args),
+        "created_after": _get_date("created_after", args),
+        "updated_after": _get_date("updated_after", args)
     }
