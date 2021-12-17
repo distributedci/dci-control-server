0dda52457376cfb6d3f810d11625dd4a83395c02
Guillaume Vincent <gvincent@redhat.com>
2021-12-17 15:36:42 +0000

    Apply black automatically
    Change-Id: Ia3eca7f3a6b592a679fd722ab60aa5c7cb99537c

--- dci/common/schemas.py
+++ dci/common/schemas.py
@@ -214,7 +214,7 @@ update_job_properties = {
     "status_reason": Properties.string,
     "configuration": Properties.string,
     "name": Properties.string,
-    "url": Properties.url
+    "url": Properties.url,
 }
 update_job_schema = {"type": "object", "properties": update_job_properties}
 
@@ -285,7 +285,7 @@ update_product_properties = {
     "name": Properties.string,
     "label": Properties.string,
     "description": Properties.string,
-    "state": Properties.enum(valid_resource_states)
+    "state": Properties.enum(valid_resource_states),
 }
 update_product_schema = {"type": "object", "properties": update_product_properties}
 
@@ -449,7 +449,7 @@ create_component_properties = {
     "state": with_default(Properties.enum(valid_resource_states), "active"),
     "data": with_default(Properties.json, {}),
     "tags": with_default(Properties.array, []),
-    "released_at": Properties.isoformat_date
+    "released_at": Properties.isoformat_date,
 }
 create_component_schema = {
     "type": "object",
@@ -469,7 +469,7 @@ update_component_properties = {
     "topic_id": Properties.uuid,
     "state": Properties.enum(valid_resource_states),
     "data": Properties.json,
-    "tags": Properties.array
+    "tags": Properties.array,
 }
 update_component_schema = {"type": "object", "properties": update_component_properties}
 
@@ -579,7 +579,7 @@ create_team_properties = {
     "name": Properties.string,
     "country": with_default(Properties.string, None),
     "state": with_default(Properties.enum(valid_resource_states), "active"),
-    "external": with_default(Properties.boolean, False)
+    "external": with_default(Properties.boolean, False),
 }
 create_team_schema = {
     "type": "object",
@@ -592,7 +592,7 @@ update_team_properties = {
     "name": Properties.string,
     "country": allow_none(Properties.string),
     "state": Properties.enum(valid_resource_states),
-    "external": Properties.boolean
+    "external": Properties.boolean,
 }
 update_team_schema = {"type": "object", "properties": update_team_properties}
 
