f85a541b54658081b9600d1a6f7f4e5b8aba8a6b
ylamgarchal <yassine.lamgarchal@redhat.com>
2022-01-14 15:26:13 +0100

    analytics: add components coverage
      - GET /analytics/tasks_components_coverage
            topic_id=<topic-id>
            team_id=<team-id>
    
      - To scope the result of a specific team, the
        user must provide a valid team id on which it
        belongs to.
    
      - If a user has the permission, it may get the
        whole set of result by providing a specific
        value for team_id=red_hat
    
    Change-Id: I8c4b6ffcab47c917584460e1f99cd0dc0233adfc

--- dci/common/schemas.py
+++ dci/common/schemas.py
@@ -615,10 +615,21 @@ analytics_task_duration_cumulated_properties = {
     "topic_id": Properties.uuid,
     "remoteci_id": Properties.uuid,
 }
-
 analytics_task_duration_cumulated = {
     "type": "object",
     "properties": analytics_task_duration_cumulated_properties,
     "required": ["topic_id", "remoteci_id"],
+    "additionalProperties": False,
+}
+
+
+analytics_task_components_coverage_properties = {
+    "topic_id": Properties.uuid,
+    "team_id": Properties.string,
+}
+analytics_task_components_coverage = {
+    "type": "object",
+    "properties": analytics_task_components_coverage_properties,
+    "required": ["topic_id", "team_id"],
     "additionalProperties": False,
 }
