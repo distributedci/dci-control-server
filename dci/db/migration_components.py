0fb276550a3fc4e76e6e1d200a36aca99b896bb0
ylamgarchal <yassine.lamgarchal@redhat.com>
2022-10-13 15:41:11 +0200

    components: add display_name and version fields
      - currently the canonical_project_name field contains
        the name and the version while the name field contains
        the version of the components, this is legacy behaviour
        to get rid of.
      - to do so, this patch introduce the fields:
          - display_name: that will only store the real name
            of the component
          - version: that will store only the version of the
            component
    
    To maintain backward compatibility the api has been modified
    to extra display_name and version from the legacy
    canonical_project_name and name fields when new components
    are created.
    
    The migration script also update old components with the
    same extraction logic.
    
    Change-Id: Ie8930f6c28b52aa5cf1c8dd9182ff48935bbb983

---
+++ dci/db/migration_components.py
@@ -0,0 +1,70 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2023 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import re
+
+
+def _get_version_delimiter(component_name):
+    delimiter = None
+    for c in [":", "@", " "]:
+        if c in component_name:
+            delimiter = c
+            break
+    return delimiter
+
+
+def _is_sha1(s):
+    pattern = r"^[a-fA-F0-9]{40}$"
+    return bool(re.match(pattern, s))
+
+
+def _get_semver(version):
+    match = re.match(r"^(\d+\.\d+\.\d+)", version)
+    if match:
+        return match.group(1)
+    else:
+        return version
+
+
+def get_component_display_name_and_version(name, canonical_project_name):
+    name_is_version = _is_sha1(name)
+
+    component_name = (
+        canonical_project_name
+        if "OpenShift" in canonical_project_name
+        or name in canonical_project_name
+        or name_is_version
+        else name
+    )
+
+    for short_name in ["RHOS", "RHEL"]:
+        if short_name in component_name:
+            version = component_name.rsplit(f"{short_name}-", 1)[-1]
+            return component_name, version
+
+    if "OpenShift" in canonical_project_name:
+        display_name = " ".join(canonical_project_name.split(" ", 2)[:2])
+        return " ".join(canonical_project_name.split(" ", 2)[:2]), name
+
+    delimiter = _get_version_delimiter(component_name)
+    if delimiter is None:
+        return component_name, ""
+
+    display_name, version = component_name.rsplit(delimiter, 1)
+    if name_is_version:
+        return f"{display_name} {name[0:7]}", name
+
+    display_name = f"{display_name} {_get_semver(version)}"
+    return display_name, version
