6f6941e0dffcccee115563d5d7d404ad1db4ef1e
ylamgarchal <yassine.lamgarchal@redhat.com>
2022-10-13 15:41:11 +0200

    components: add display_name and version fields
      - currently the canonical_project_name field contains
        the name and the version while the name field contains
        the version of the components, this is legacy behaviour
        to get rid of.
      - to do so, this patch introduce the fields:
          - display_name: that will only store the real name
            of the component
          - version: that will store only the version of the
            component
    
    To maintain backward compatibility the api has been modified
    to extra display_name and version from the legacy
    canonical_project_name and name fields when new components
    are created.
    
    The migration script also update old components with the
    same extraction logic.
    
    Change-Id: Ie8930f6c28b52aa5cf1c8dd9182ff48935bbb983

---
+++ dci/alembic/versions/46dff7ed04c2_migrate_new_components_fields.py
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2023 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Migrate new components fields
+
+Revision ID: 46dff7ed04c2
+Revises: 66680dcf2c88
+Create Date: 2023-01-11 15:37:58.104746
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "46dff7ed04c2"
+down_revision = "66680dcf2c88"
+branch_labels = None
+depends_on = None
+
+from dci.api.v1.components import get_component_display_name, get_component_version
+from dci.db import models2
+
+from alembic import op
+from sqlalchemy.orm.session import Session
+from sqlalchemy import exc
+
+
+def upgrade():
+    session = Session(op.get_bind())
+
+    query = session.query(models2.Component)
+    query = query.order_by(models2.Component.created_at.asc())
+    offset = 0
+    limit = 100
+    query = query.limit(limit)
+
+    try:
+        while True:
+            query = query.offset(offset)
+
+            components = query.all()
+            if not components:
+                break
+            for c in components:
+                name = c.canonical_project_name or c.name
+                if not c.display_name:
+                    c.display_name = get_component_display_name(name)
+                if not c.version:
+                    c.version = get_component_version(name)
+            offset += limit
+    except exc.IntegrityError:
+        session.rollback()
+        session.close()
+    except Exception:
+        session.rollback()
+        session.close()
+
+    session.commit()
+    session.close()
+
+
+def downgrade():
+    pass
