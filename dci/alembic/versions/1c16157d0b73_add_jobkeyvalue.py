f2402673d7977cf714ab1d822d21317a780371a8
ylamgarchal <yassine.lamgarchal@redhat.com>
2023-01-23 16:19:17 +0100

    wip: kv
    Change-Id: I6a071fbb648f82c22c552182fcdacf8d3fa378cd

---
+++ dci/alembic/versions/1c16157d0b73_add_jobkeyvalue.py
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2023 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Add JobKeyValue
+
+Revision ID: 1c16157d0b73
+Revises: fd04b7d20477
+Create Date: 2023-01-25 13:37:09.897416
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "1c16157d0b73"
+down_revision = "fd04b7d20477"
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql as pg
+
+
+def upgrade():
+    op.create_table(
+        "jobs_keys_values",
+        sa.Column("key", sa.String(255), nullable=False, primary_key=True),
+        sa.Column("value", sa.String(255), nullable=False, primary_key=True),
+        sa.Column(
+            "job_id",
+            pg.UUID(as_uuid=True),
+            sa.ForeignKey("jobs.id", ondelete="CASCADE"),
+            nullable=False,
+            primary_key=True,
+        ),
+        sa.UniqueConstraint("job_id", "key", "value", name="jobs_keys_values_jkv"),
+    )
+    op.create_index("jobs_keys_values_key_idx", "jobs_keys_values", ["key"])
+
+
+def downgrade():
+    pass
---
+++ dci/api/v1/jobs_kv.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import flask
+from flask import json
+import logging
+
+from dci.api.v1 import api
+from dci.api.v1 import base
+from dci import decorators
+from dci.common import exceptions as dci_exc
+from dci.common import utils
+from dci.db import models2
+
+
+logger = logging.getLogger(__name__)
+
+
+@api.route("/jobs/<uuid:job_id>/kv", methods=["POST"])
+@decorators.login_required
+def add_kv_to_job(user, job_id):
+    values = flask.request.json
+
+    job = base.get_resource_orm(models2.Job, job_id)
+
+    if user.is_not_in_team(job.team_id) and user.is_not_epm():
+        raise dci_exc.Unauthorized()
+
+    values["job_id"] = job_id
+    jkv = base.create_resource_orm(models2.JobKeyValue, values)
+
+    return flask.Response(
+        json.dumps({"kv": jkv}),
+        201,
+        content_type="application/json",
+    )
