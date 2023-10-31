fb94051b3cc814f902c4f0956fe4a23f4bfcfe06
ylamgarchal <yassine.lamgarchal@redhat.com>
2023-10-31 15:33:54 +0100

    alembic: attach components to their product
    Change-Id: Iacf0107a97b08a8ad32c1297a81d4e3490960156

---
+++ dci/alembic/versions/151c7129cc09_add_product_to_old_components.py
@@ -0,0 +1,86 @@
+#
+# Copyright (C) 2023 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""set product_id value to old components
+
+Revision ID: 151c7129cc09
+Revises: 7c1538bc073b
+Create Date: 2023-07-21 02:59:38.933407
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "151c7129cc09"
+down_revision = "7c1538bc073b"
+branch_labels = None
+depends_on = None
+
+from dci.db import models2
+from alembic import op
+from sqlalchemy.orm.session import Session
+from sqlalchemy import exc
+import sys
+
+_topic_to_product = {}
+
+
+def upgrade():
+    s = Session(op.get_bind())
+    try:
+        offset = 0
+        limit = 3000
+        while True:
+            query = (
+                s.query(models2.Component)
+                .offset(offset)
+                .limit(limit)
+            )
+            components = query.all()
+            if not components:
+                break
+            for c in components:
+                if c.topic_id not in _topic_to_product:
+                    t = (
+                        s.query(models2.Topic)
+                        .filter(models2.Topic.id == c.topic_id)
+                        .one()
+                    )
+                    p = (
+                        s.query(models2.Product)
+                        .filter(models2.Product.id == t.product_id)
+                        .one()
+                    )
+                    _topic_to_product[c.topic_id] = p
+                p = _topic_to_product[c.topic_id]
+                p.components.append(c)
+                s.add(p)
+            print("flush\n")
+            s.flush()
+            offset += limit
+    except exc.IntegrityError as ie:
+        print("rollback" + str(ie))
+        s.rollback()
+        s.close()
+        sys.exit(1)
+    except Exception as e:
+        print("Exception" + str(e))
+        s.rollback()
+        s.close()
+        sys.exit(1)
+    s.commit()
+
+
+def downgrade():
+    pass
