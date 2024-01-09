56e04f8e299fde5e7b18ca3b42c8ccf7a640790e
ylamgarchal <yassine.lamgarchal@redhat.com>
2024-01-09 02:16:07 +0100

    analytics: add jobs endpoint
        - POST /analytics/jobs query=<query>
    
    the query must be a query_string
    elasticsearch query
    
    Change-Id: I26eb2a7ac06aea34af127afcad7730123dc16a0f

--- dci/api/v1/analytics.py
+++ dci/api/v1/analytics.py
@@ -30,6 +30,7 @@ from dci.common.schemas import (
     analytics_task_components_coverage,
     analytics_tasks_junit,
     analytics_tasks_pipelines_status,
+    analytics_jobs,
     check_json_is_valid,
 )
 from dci.dci_config import CONFIG
@@ -225,6 +226,40 @@ def tasks_pipelines_status(user):
     try:
         res = requests.post(
             "%s/analytics/pipelines_status" % CONFIG["ANALYTICS_URL"],
+            headers={"Content-Type": "application/json"},
+            json=values,
+        )
+
+        if res.status_code == 200:
+            return flask.jsonify(res.json())
+        else:
+            logger.error("analytics error: %s" % str(res.text))
+            return flask.Response(
+                json.dumps({"error": "error with backend service: %s" % str(res.text)}),
+                res.status_code,
+                content_type="application/json",
+            )
+    except ConnectionError as e:
+        logger.error("analytics connection error: %s" % str(e))
+        return flask.Response(
+            json.dumps({"error": "connection error with backend service: %s" % str(e)}),
+            503,
+            content_type="application/json",
+        )
+
+
+@api.route("/analytics/jobs", methods=["POST"])
+@decorators.login_required
+def tasks_jobs(user):
+    values = flask.request.json
+    check_json_is_valid(analytics_jobs, values)
+
+    if user.is_not_super_admin():
+        raise dci_exc.Unauthorized()
+
+    try:
+        res = requests.post(
+            "%s/jobs" % CONFIG["ANALYTICS_URL"],
             headers={"Content-Type": "application/json"},
             json=values,
         )
