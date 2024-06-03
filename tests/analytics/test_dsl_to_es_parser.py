03bb3ac9bac92dfabaecf48ed6ee215fd52e64b6
Guillaume Vincent <gvincent@redhat.com>
2024-06-03 15:26:58 +0200

    WIP dsl_to_es_parser
    Change-Id: Ifd8e4d672c7b38ddc220241bf618eefd5d3a224a

---
+++ tests/analytics/test_dsl_to_es_parser.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2024 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def parse_dsl_to_es(query):
+    pass
+
+
+def test_simple_query():
+    query = "name=foo"
+    expected_es_query = {"query": {"match": {"name": "foo"}}}
+    assert parse_dsl_to_es(query) == expected_es_query
+
+
+def test_simple_query_with_sort():
+    query = "name=foo sort=created_at:desc"
+    expected_es_query = {
+        "query": {"match": {"name": "foo"}},
+        "sort": [
+            {"created_at": {"order": "desc"}},
+        ],
+    }
+    assert parse_dsl_to_es(query) == expected_es_query
+
+def test_simple_query_with_nested_value():
+    query = "remoteci.name=foo"
+    expected_es_query = {
+        "query": {"match": {"remoteci.name": "foo"}},
+    }
+    assert parse_dsl_to_es(query) == expected_es_query
