2d9365962070076fa47a73915f33cdf02b249e3f
ylamgarchal <yassine.lamgarchal@redhat.com>
2025-11-05 00:42:30 +0100

    ensure inactive remotecis cannot do api calls

--- tests/auth/test_hmac_mechanism.py
+++ tests/auth/test_hmac_mechanism.py
@@ -14,6 +14,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from dci import auth_mechanism
+import flask
+
 
 def test_hmac_mechanism_api_get_jobs_remoteci(hmac_client_team1):
     jobs_request = hmac_client_team1.get("/api/v1/jobs")
@@ -28,3 +31,40 @@ def test_hmac_mechanism_params(hmac_client_team1):
 def test_hmac_mechanism_api_get_jobs_feeder(hmac_client_feeder):
     jobs_request = hmac_client_feeder.get("/api/v1/jobs")
     assert jobs_request.status_code == 200
+
+
+def test_nrt_build_identity_with_remoteci_inactive(
+    app,
+    session,
+    team_admin_id,
+    team_redhat_id,
+    team_epm_id,
+    hmac_client_team1,
+    team1_remoteci,
+):
+    with app.app_context():
+        flask.g.team_admin_id = team_admin_id
+        flask.g.team_redhat_id = team_redhat_id
+        flask.g.team_epm_id = team_epm_id
+        flask.g.session = session
+        hm = auth_mechanism.HmacMechanism(None)
+        bi = hm.build_identity(
+            client_info={"client_type": "remoteci", "client_id": team1_remoteci["id"]}
+        )
+        assert bi is not None
+        t = hmac_client_team1.get("/api/v1/remotecis/" + team1_remoteci["id"]).data[
+            "remoteci"
+        ]
+        data = {"state": "inactive"}
+        r = hmac_client_team1.put(
+            "/api/v1/remotecis/" + team1_remoteci["id"],
+            data=data,
+            headers={"If-match": t["etag"]},
+        )
+        assert r.status_code == 200
+        assert r.data["remoteci"]["state"] == "inactive"
+
+        bi = hm.build_identity(
+            client_info={"client_type": "remoteci", "client_id": team1_remoteci["id"]}
+        )
+        assert bi is None
